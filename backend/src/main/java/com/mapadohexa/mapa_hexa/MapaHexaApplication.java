03c91295eb4b98fcbcd3e904dc57e7396dad73b9
Caio <07soaresscaio@gmail.com>
2026-03-31 00:06:29 -0300

    inicia projeto spring

---
+++ backend/src/main/java/com/mapadohexa/mapa_hexa/MapaHexaApplication.java
@@ -0,0 +1,13 @@
+package com.mapadohexa.mapa_hexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapaHexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapaHexaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mapadohexa/mapa_hexa/MapaHexaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapadohexa.mapa_hexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapaHexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
